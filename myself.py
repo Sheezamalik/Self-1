4df333eab9583cda476095694d97fa26ba249420
Sheezamalik <sheezamalik533@gmail.com>
2024-08-04 02:00:20 +0500

    Add files via upload

---
+++ myself.py
@@ -0,0 +1,29 @@
+#myself essay
+
+#introduction
+print("Hello, my name is Sheeza Malik and I am a computer student.")
+print("I am interested in learning more about computer science and its applications.")
+#Hoobies
+print("My hobbies include:")
+print("- Reading")
+print("- Hiking")
+print("- singing")
+# Fave language
+print("My favorite programming languages are:")
+print("- Python")
+print("- Java")
+print("- C++")
+# Goals
+print("My career goals are to become a software engineer and work on projects that make a positive impact on society.")
+#Fun fact
+print("A fun fact about me is that I love trying new things")
+
+#try An input
+name = input("What is your name? ")
+print("Hello, " + name)
+#age
+age = input("How old are you? ")
+print("You are " + age + " years old")
+
+#fav color
+print("My favorite color is Green")
